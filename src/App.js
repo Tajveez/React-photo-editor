2d1732614f6179f455abe929f5f94476f5044297
Tajveez <tajveez786@gmail.com>
2020-09-19 23:09:30 +0500

    inital

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import Slider from "./components/Slider";
 function App() {
   return (
     <div className="container">
-      <div className="main-image">
-        <div className="sidbar"></div>
-        <Slider />
-      </div>
+      <div className="main-image">Image</div>
+      <div className="sidebar">Sidebar</div>
+      <Slider />
     </div>
   );
 }
